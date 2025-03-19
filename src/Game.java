e2f25b18aa5a1f60cab527f016d99289fd7f262b
BarnyTheGOAT <awesomebillybob0@gmail.com>
2025-03-19 10:52:33 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -12,15 +12,23 @@ public class Game extends JPanel implements ActionListener, KeyListener{
 	private double pl1X, pl1XVel, pl1Y, pl1YVel, pl2X, pl2XVel, pl2Y, pl2YVel;
 	private double gameX, gameY, gameWidth, gameHeight;
 	private int floor;
-	private Timer gameTM;
+	private Timer gameTM, gameStartTM;
 	private Image player1Image, player2Image;
+	
+	private int player1Width = player1Image.getWidth(null);
+	private int player1Height = player1Image.getHeight(null);
+	private int player1Size = 2;
+
+	private int player2Width = player2Image.getWidth(null);
+	private int player2Height = player2Image.getHeight(null);
+	private int player2Size = 2;
 
 	public Game() {
 
 		ImageIcon player1Image = new ImageIcon("src/player1.png");
 		this.player1Image = player1Image.getImage();
-		
-		ImageIcon player2Image = new ImageIcon("src/player2	.png");
+
+		ImageIcon player2Image = new ImageIcon("src/player2.png");
 		this.player2Image = player2Image.getImage();
 
 		gameX = gameY = pl1XVel = pl1YVel = pl2XVel = pl2YVel = 0;
@@ -38,19 +46,11 @@ public class Game extends JPanel implements ActionListener, KeyListener{
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		int player1Width = player1Image.getWidth(null);
-		int player1Height = player1Image.getHeight(null);
-		int player1Size = 2;
-		
-		int player2Width = player2Image.getWidth(null);
-		int player2Height = player2Image.getHeight(null);
-		int player2Size = 2;
-		
+
 		g.drawImage(player1Image, (int) pl1X - (player1Width / player1Size / 2), 
 				(int) pl1Y - (player1Height / player1Size), player1Image.getWidth(null) / player1Size, 
 				player1Image.getHeight(null) / player1Size, null);
-		
+
 
 		g.drawImage(player2Image, (int) pl2X - (player2Width / player2Size / 2), 
 				(int) pl2Y - (player2Height / player2Size), player2Image.getWidth(null) / player2Size, 
@@ -107,76 +107,83 @@ public class Game extends JPanel implements ActionListener, KeyListener{
 
 	public void actionPerformed(ActionEvent e) {
 
-		requestFocusInWindow();
+		if (e.getSource() == gameTM) {
+			requestFocusInWindow();
 
-		double jumpHeight = -20;
-		double speed = 2;
+			double jumpHeight = -20;
+			double speed = 2;
 
-		// All of player one's code(WASD)
-		if (kw && pl1Y >= floor) {
-			pl1YVel = jumpHeight;
-		} if (ka) {
-			pl1XVel -= speed;
-		} if (kd) {
-			pl1XVel += speed;
+			// All of player one's code(WASD)
+			if (kw && pl1Y >= floor) {
+				pl1YVel = jumpHeight;
+			} if (ka) {
+				pl1XVel -= speed;
+			} if (kd) {
+				pl1XVel += speed;
+			}
+
+			pl1XVel *= 0.9;
+
+			pl1X += pl1XVel;
+			pl1Y += pl1YVel;
+
+			if (pl1Y < floor) {
+				pl1YVel += 1;
+			} else {
+				pl1YVel = 0;
+				pl1Y = floor;
+			}
+
+			if (pl1X >= getWidth()) {
+				pl1X = getWidth();
+			} else if (pl1X <= 0) {
+				pl1X = 0;
+			}
+
+			// All of player two's code(↑←→↓)
+			if (kup && pl2Y >= floor) {
+				pl2YVel = jumpHeight;
+			} if (kleft) {
+				pl2XVel -= speed;
+			} if (kright) {
+				pl2XVel += speed;
+			}
+
+			pl2XVel *= 0.9;
+
+			pl2X += pl2XVel;
+			pl2Y += pl2YVel;
+
+			System.out.println(pl2X);
+
+			if (pl2Y < floor) {
+				pl2YVel += 1;
+			} else {
+				pl2YVel = 0;
+				pl2Y = floor;
+			}
+
+			if (pl2X + player2Width/2/player2Size >= getWidth()) {
+				pl2X = getWidth();
+			} else if (pl2X - player2Width/2/player2Size <= 0) {
+				pl2X = 0;
+			}
+
+
+			repaint();
+		} else if (e.getSource() == gameStartTM) {
+			gameTM.start();
+			gameStartTM.stop();
 		}
-
-		pl1XVel *= 0.9;
-
-		pl1X += pl1XVel;
-		pl1Y += pl1YVel;
-
-		if (pl1Y < floor) {
-			pl1YVel += 1;
-		} else {
-			pl1YVel = 0;
-			pl1Y = floor;
-		}
-		
-		if (pl1X >= getWidth()) {
-			pl1X = getWidth();
-		} else if (pl1X <= 0) {
-			pl1X = 0;
-		}
-
-		// All of player two's code(↑←→↓)
-		if (kup && pl2Y >= floor) {
-			pl2YVel = jumpHeight;
-		} if (kleft) {
-			pl2XVel -= speed;
-		} if (kright) {
-			pl2XVel += speed;
-		}
-
-		pl2XVel *= 0.9;
-
-		pl2X += pl2XVel;
-		pl2Y += pl2YVel;
-		
-		System.out.println(pl2Y);
-
-		if (pl2Y < floor) {
-			pl2YVel += 1;
-		} else {
-			pl2YVel = 0;
-			pl2Y = floor;
-		}
-		
-		if (pl2X >= getWidth()) {
-			pl2X = getWidth();
-		} else if (pl2X <= 0) {
-			pl2X = 0;
-		}
-			
-
-		repaint();
 	}
 
 	public void setupWindow() {
 		JFrame f = new JFrame("Game");
 
 		gameTM = new Timer(10, this);
-		gameTM.start();
+		gameStartTM = new Timer(1000, this);
+		
+		gameStartTM.start();
 
 		addKeyListener(this);
 
