dd099c31ebff432aee40b7ad32c59ff69b5227b6
AaryaTeja <130732069+AaryaTeja@users.noreply.github.com>
2025-03-20 09:29:49 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,213 +1,212 @@
-/* Arnav Jaiswal && Aaryateja Addala
- * Mar 14
- * the entire game
- */
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Game extends JPanel implements ActionListener, KeyListener{
-	// detect whether these keys are pressed
-	private boolean kleft,kright, lright, kup, kdown, kw, ka, ks, kd;
-	private double pl1X, pl1XVel, pl1Y, pl1YVel, pl2X, pl2XVel, pl2Y, pl2YVel;
-	private double gameX, gameY, gameWidth, gameHeight;
-	private int floor;
-	private Timer gameTM, gameStartTM;
-	private Image player1Image, player2Image;
-	
-	private int player1Width;
-	private int player1Height;
-	private int player1Size;
 
-	private int player2Width;
-	private int player2Height;
-	private int player2Size;
+public class Game extends JPanel implements ActionListener, KeyListener {
+    // detect whether these keys are pressed
+    private boolean kleft, kright, kup, kdown, kw, ka, ks, kd;
+    private double pl1X, pl1XVel, pl1Y, pl1YVel, pl2X, pl2XVel, pl2Y, pl2YVel;
+    private double gameX, gameY, gameWidth, gameHeight;
+    private int floor;
+    private Timer gameTM, gameStartTM;
+    private Image player1Image, player2Image, backgroundImage;
 
-	public Game() {
+    private int player1Width;
+    private int player1Height;
+    private int player1Size;
 
-		ImageIcon player1Image = new ImageIcon("src/player1.png");
-		this.player1Image = player1Image.getImage();
+    private int player2Width;
+    private int player2Height;
+    private int player2Size;
 
-		ImageIcon player2Image = new ImageIcon("src/player2.png");
-		this.player2Image = player2Image.getImage();
+    public Game() {
+        ImageIcon player1Image = new ImageIcon("src/player1.png");
+        this.player1Image = player1Image.getImage();
 
-		gameX = gameY = pl1XVel = pl1YVel = pl2XVel = pl2YVel = 0;
+        ImageIcon player2Image = new ImageIcon("src/player2.png");
+        this.player2Image = player2Image.getImage();
 
-		gameWidth = 800;
-		gameHeight = 600;
+        // Load the background image
+        ImageIcon backgroundIcon = new ImageIcon("src/Background.jpg");
+        this.backgroundImage = backgroundIcon.getImage();
 
-		floor = 500;
+        gameX = gameY = pl1XVel = pl1YVel = pl2XVel = pl2YVel = 0;
 
+        gameWidth = 800;
+        gameHeight = 600;
 
-		pl1X = 125;
-		pl2X = 675;
-		pl1Y = pl2Y = floor;
-		
+        floor = 500;
 
-		player1Width = this.player1Image.getWidth(null);
-		player1Height = this.player1Image.getHeight(null);
-		player1Size = 2;
+        pl1X = 125;
+        pl2X = 675;
+        pl1Y = pl2Y = floor;
 
-		player2Width = this.player2Image.getWidth(null);
-		player2Height = this.player2Image.getHeight(null);
-		player2Size = 2;
-	}
+        player1Width = this.player1Image.getWidth(null);
+        player1Height = this.player1Image.getHeight(null);
+        player1Size = 2;
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+        player2Width = this.player2Image.getWidth(null);
+        player2Height = this.player2Image.getHeight(null);
+        player2Size = 2;
+    }
 
-		g.drawImage(player1Image, (int) pl1X - (player1Width / player1Size / 2), 
-				(int) pl1Y - (player1Height / player1Size), player1Image.getWidth(null) / player1Size, 
-				player1Image.getHeight(null) / player1Size, null);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        // Draw the background image
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
 
-		g.drawImage(player2Image, (int) pl2X - (player2Width / player2Size / 2), 
-				(int) pl2Y - (player2Height / player2Size), player2Image.getWidth(null) / player2Size, 
-				player2Image.getHeight(null) / player2Size, null);
+        // Draw player 1
+        g.drawImage(player1Image, (int) pl1X - (player1Width / player1Size / 2),
+                (int) pl1Y - (player1Height / player1Size), player1Image.getWidth(null) / player1Size,
+                player1Image.getHeight(null) / player1Size, null);
 
-	}	
+        // Draw player 2
+        g.drawImage(player2Image, (int) pl2X - (player2Width / player2Size / 2),
+                (int) pl2Y - (player2Height / player2Size), player2Image.getWidth(null) / player2Size,
+                player2Image.getHeight(null) / player2Size, null);
+    }
 
-	public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {
+    }
 
-	}
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            kleft = true;
+        } else if (key == KeyEvent.VK_RIGHT) {
+            kright = true;
+        } else if (key == KeyEvent.VK_UP) {
+            kup = true;
+        } else if (key == KeyEvent.VK_DOWN) {
+            kdown = true;
+        } else if (key == KeyEvent.VK_W) {
+            kw = true;
+        } else if (key == KeyEvent.VK_A) {
+            ka = true;
+        } else if (key == KeyEvent.VK_S) {
+            ks = true;
+        } else if (key == KeyEvent.VK_D) {
+            kd = true;
+        }
+    }
 
-	public void keyPressed(KeyEvent e) {		
-		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_LEFT) {
-			kleft = true;
-		} else if (key == KeyEvent.VK_RIGHT) {
-			kright = true;
-		} else if (key == KeyEvent.VK_UP) {
-			kup = true;
-		} else if (key == KeyEvent.VK_DOWN) {
-			kdown = true;
-		} else if (key == KeyEvent.VK_W) {
-			kw = true;
-		} else if (key == KeyEvent.VK_A) {
-			ka = true;
-		} else if (key == KeyEvent.VK_S) {
-			ks = true;
-		} else if (key == KeyEvent.VK_D) {
-			kd = true;
-		}
-	}
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            kleft = false;
+        } else if (key == KeyEvent.VK_RIGHT) {
+            kright = false;
+        } else if (key == KeyEvent.VK_UP) {
+            kup = false;
+        } else if (key == KeyEvent.VK_DOWN) {
+            kdown = false;
+        } else if (key == KeyEvent.VK_W) {
+            kw = false;
+        } else if (key == KeyEvent.VK_A) {
+            ka = false;
+        } else if (key == KeyEvent.VK_S) {
+            ks = false;
+        } else if (key == KeyEvent.VK_D) {
+            kd = false;
+        }
+    }
 
-	public void keyReleased(KeyEvent e) {
-		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_LEFT) {
-			kleft = false;
-		} else if (key == KeyEvent.VK_RIGHT) {
-			kright = false;
-		} else if (key == KeyEvent.VK_UP) {
-			kup = false;
-		} else if (key == KeyEvent.VK_DOWN) {
-			kdown = false;
-		} else if (key == KeyEvent.VK_W) {
-			kw = false;
-		} else if (key == KeyEvent.VK_A) {
-			ka = false;
-		} else if (key == KeyEvent.VK_S) {
-			ks = false;
-		} else if (key == KeyEvent.VK_D) {
-			kd = false;
-		}
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == gameTM) {
+            floor = 9 * getHeight() / 10;
 
-	}
+            requestFocusInWindow();
 
-	public void actionPerformed(ActionEvent e) {
+            double jumpHeight = -20;
+            double speed = 2;
 
-		if (e.getSource() == gameTM) {
-			
-			floor = 9 * getHeight() / 10;
-			
-			requestFocusInWindow();
+            // All of player one's code(WASD)
+            if (kw && pl1Y >= floor) {
+                pl1YVel = jumpHeight;
+            }
+            if (ka) {
+                pl1XVel -= speed;
+            }
+            if (kd) {
+                pl1XVel += speed;
+            }
 
-			double jumpHeight = -20;
-			double speed = 2;
+            pl1XVel *= 0.9;
 
-			// All of player one's code(WASD)
-			if (kw && pl1Y >= floor) {
-				pl1YVel = jumpHeight;
-			} if (ka) {
-				pl1XVel -= speed;
-			} if (kd) {
-				pl1XVel += speed;
-			}
+            pl1X += pl1XVel;
+            pl1Y += pl1YVel;
 
-			pl1XVel *= 0.9;
+            if (pl1Y < floor) {
+                pl1YVel += 1;
+            } else {
+                pl1YVel = 0;
+                pl1Y = floor;
+            }
 
-			pl1X += pl1XVel;
-			pl1Y += pl1YVel;
+            if (pl1X + player1Width / 2 / player1Size >= getWidth()) {
+                pl1X = getWidth() - player1Width / 2 / player1Size;
+            } else if (pl1X - player1Width / 2 / player1Size <= 0) {
+                pl1X = player1Width / 2 / player1Size;
+            }
 
-			if (pl1Y < floor) {
-				pl1YVel += 1;
-			} else {
-				pl1YVel = 0;
-				pl1Y = floor;
-			}
+            // All of player two's code(↑←→↓)
+            if (kup && pl2Y >= floor) {
+                pl2YVel = jumpHeight;
+            }
+            if (kleft) {
+                pl2XVel -= speed;
+            }
+            if (kright) {
+                pl2XVel += speed;
+            }
 
-			if (pl1X + player1Width/2/player1Size >= getWidth()) {
-				pl1X = getWidth() - player1Width/2/player1Size;
-			} else if (pl1X - player1Width/2/player1Size <= 0) {
-				pl1X = player1Width/2/player1Size;
-			}
+            pl2XVel *= 0.9;
 
-			// All of player two's code(↑←→↓)
-			if (kup && pl2Y >= floor) {
-				pl2YVel = jumpHeight;
-			} if (kleft) {
-				pl2XVel -= speed;
-			} if (kright) {
-				pl2XVel += speed;
-			}
+            pl2X += pl2XVel;
+            pl2Y += pl2YVel;
 
-			pl2XVel *= 0.9;
+            System.out.println(pl2X);
 
-			pl2X += pl2XVel;
-			pl2Y += pl2YVel;
+            if (pl2Y < floor) {
+                pl2YVel += 1;
+            } else {
+                pl2YVel = 0;
+                pl2Y = floor;
+            }
 
-			System.out.println(pl2X);
+            if (pl2X + player2Width / 2 / player2Size >= getWidth()) {
+                pl2X = getWidth() - player2Width / 2 / player2Size;
+            } else if (pl2X - player2Width / 2 / player2Size <= 0) {
+                pl2X = player2Width / 2 / player2Size;
+            }
 
-			if (pl2Y < floor) {
-				pl2YVel += 1;
-			} else {
-				pl2YVel = 0;
-				pl2Y = floor;
-			}
+            repaint();
+        } else if (e.getSource() == gameStartTM) {
+            gameTM.start();
+            gameStartTM.stop();
+        }
+    }
 
-			if (pl2X + player2Width/2/player2Size >= getWidth()) {
-				pl2X = getWidth() - player2Width/2/player2Size;
-			} else if (pl2X - player2Width/2/player2Size <= 0) {
-				pl2X = player2Width/2/player2Size;
-			}
+    public void setupWindow() {
+        JFrame f = new JFrame("Game");
 
+        gameTM = new Timer(10, this);
+        gameStartTM = new Timer(500, this);
 
-			repaint();
-		} else if (e.getSource() == gameStartTM) {
-			gameTM.start();
-			gameStartTM.stop();
-		}
-	}
+        gameStartTM.start();
 
-	public void setupWindow() {
-		JFrame f = new JFrame("Game");
+        addKeyListener(this);
 
-		gameTM = new Timer(10, this);
-		gameStartTM = new Timer(500, this);
-		
-		gameStartTM.start();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(800, 600);
+        f.setLocationRelativeTo(null);
+        f.add(this);
+        f.setVisible(true);
+    }
 
-		addKeyListener(this);
-
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(800, 600);
-		f.setLocationRelativeTo(null);
-		f.add(this);
-		f.setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.setupWindow();
-	}
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.setupWindow();
+    }
 }
