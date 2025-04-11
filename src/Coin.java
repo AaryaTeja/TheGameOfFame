53ff6029203a2837b19117f627cee9637badadb3
AaryaTeja <130732069+AaryaTeja@users.noreply.github.com>
2025-04-11 09:16:16 -0700

    Update Coin.java

--- src/Coin.java
+++ src/Coin.java
@@ -1,16 +1,15 @@
-
 class Coin {
-	private int x, y;
-	private boolean collected;
+    private int x, y;
+    private boolean collected;
 
-	public Coin(int x, int y) {
-		this.x = x;
-		this.y = y;
-		this.collected = false;
-	}
+    public Coin(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.collected = false;
+    }
 
-	public int getX() { return x; }
-	public int getY() { return y; }
-	public boolean isCollected() { return collected; }
-	public void collect() { collected = true; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public boolean isCollected() { return collected; }
+    public void collect() { collected = true; }
 }
